91d24fcd442f94bd9c2147d5324698532c908be3
Zac Arellano <zaccc@email.arizona.edu>
2017-02-09 10:37:11 -0700

    add tests to Array.js

--- data-structures/Array.js
+++ data-structures/Array.js
@@ -52,13 +52,24 @@ class Array {
 /*  ------------------------------------------------------  */
 
 
+const assert = require('assert')
 const a = new Array()
+
+assert.equal(a.size(), 0)
 a.add(1)
 a.add(23)
 a.add(99)
 a.add(100)
 a.add('hi')
-console.log(a)
+assert.equal(a.size(), 5)
+assert.equal(a.getAtIndex(0), 1)
+assert.equal(a.getAtIndex(4), 'hi')
+a.add(100)
+assert.equal(a.size(), 6)
+a.remove(100)
+assert.equal(a.size(), 4)
+assert.equal(a.search('hi'), 3)
+assert.equal(a.search('not in array'), null)
 
 
 /*  ------------------------------------------------------  */
