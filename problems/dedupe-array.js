4d2a23c096ff356ab8c358e857e4b28ace3c765d
Zac Arellano <zaccc@email.arizona.edu>
2017-04-29 18:51:55 -0700

    add dedupe-array.js

---
+++ problems/dedupe-array.js
@@ -0,0 +1,41 @@
+
+/*  ------------------------------------------------------  */
+/*  ------------------------------------------------------  */
+
+//  take 1. - extra space (array)
+const dedupeArray = (arr) => {
+  const result = []
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i]
+    if (!result.includes(item)) {
+      result.push(item)
+    }
+  }
+  return result
+}
+
+const dedupeArray2 = (arr) => {
+  const hash = {}
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i]
+    if (!hash[item]) {
+      hash[item] = true
+    }
+  }
+  return Object.keys(hash).map(Number)
+}
+
+/*  ------------------------------------------------------  */
+/*  ------------------------------------------------------  */
+
+const assert = require('assert')
+
+assert.deepStrictEqual(dedupeArray([]), [])
+assert.deepStrictEqual(dedupeArray([1,1,1]), [1])
+assert.deepStrictEqual(dedupeArray([1,2,3,1,2]), [1,2,3])
+assert.deepStrictEqual(dedupeArray2([]), [])
+assert.deepStrictEqual(dedupeArray2([1,1,1]), [1])
+assert.deepStrictEqual(dedupeArray2([1,2,3,1,2]), [1,2,3])
+
+/*  ------------------------------------------------------  */
+/*  ------------------------------------------------------  */
